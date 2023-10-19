259824a9a23de77326efa7eb4745fe430421c217
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-10-18 23:01:49 -0300

    Update Aula03.js

--- LogicaProgramacao-Algoritmos-EstruturaDados/Aula03.js
+++ LogicaProgramacao-Algoritmos-EstruturaDados/Aula03.js
@@ -5,7 +5,7 @@ while ( i < 10000 ) { // Ativamos um loop enquanto i < dez mil, e atualizamos i 
     if (i % 3 === 0) { // Condição TRUE para divisão de resto zero para múltiplos de 3... Três iguais representa comparação de Valor E também Type.
         console.log(`O valor de i, no caso ${i}, é múltiplo de 3, pois (i % 3) tem como resto zero, então é divisível por três.`) // Usando format-string no console.log...
     } else { // Condição FALSE que acontecerá em casos em que a divisão ( i % 3 !== 0 )
-        console.log(`${i}`) // Retorno visual do console.log com o valor atual do i
+        console.log(`O valor ${i} não é múltiplo de 3.`) // Retorno visual do console.log com o valor atual do i
     }
     i++ // Incremento do valor de i, a fim de evitar o loop infinito (sem condição de saída)
 } 
